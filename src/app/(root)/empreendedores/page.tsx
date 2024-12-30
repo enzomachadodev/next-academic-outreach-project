e70501c5964d8145819dc3a384dff1d299b55579
Enzo Machado <enzo653fm@gmail.com>
2024-12-29 23:08:09 -0300

    feat: create Empreendedores page

---
+++ src/app/(root)/empreendedores/page.tsx
@@ -0,0 +1,19 @@
+import React from "react";
+
+const EmpreendedoresPage = () => {
+  return (
+    <div>
+      <section className="flex min-h-[530px] w-full flex-col items-center justify-center bg-primary px-6 py-10 text-center text-primary-foreground">
+        <h1 className="heading">
+          Apresente Sua Empresa, Conecte-se com Empreendedores!
+        </h1>
+        <p className="sub-heading">
+          Excepteur dolor ad tempor velit nisi commodo irure minim ut ad anim
+          Lorem sunt id.
+        </p>
+      </section>
+    </div>
+  );
+};
+
+export default EmpreendedoresPage;
